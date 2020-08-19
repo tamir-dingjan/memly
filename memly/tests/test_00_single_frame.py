6c3b85b85caf3aa8b8878e8f26b9de5a8b28eee4
Tamir Dingjan <tamir.dingjan@gmail.com>
2020-08-19 16:56:15 +0300

    Corrections to test_00 assertions

--- memly/tests/test_00_single_frame.py
+++ memly/tests/test_00_single_frame.py
@@ -11,24 +11,28 @@ import memly
 
 # Setup access to datafiles
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-traj = os.path.join(THIS_DIR, "data/1.pdb")
+traj = os.path.join(THIS_DIR, "data/1.xtc")
 top = os.path.join(THIS_DIR, "data/1.pdb")
 
 x = memly.Membrane(traj, top, load=True)
 # assert len(x.leaflets[0]["upper"]) == 168
 # assert np.max(x.leaflets[0]["lower"]) == 335
 
-np.testing.assert_allclose(np.round(memly.membrane.get_lipid_vector(x.sim[0], x.hg_particles_by_res, x.lipid_particles_by_res, 0),3), np.asarray([-0.476, 0.743, -0.687]))
+np.testing.assert_allclose(np.round(memly.membrane.get_lipid_vector(x.sim[0],
+                                                                    x.hg_particles_by_res,
+                                                                    x.lipid_particles_by_res,
+                                                                    0),
+                                    3),
+                           np.asarray([0.003,  0.486, -1.064]))
 
 assert len(x.raw_leaflets) == len(x.sim)
-assert len(x.raw_leaflets[0][0]) == 168
-assert np.max(x.raw_leaflets[0][0]) == 199
-assert np.median(x.raw_leaflets[0][0]) == 83.5
-assert np.min(x.raw_leaflets[0][0]) == 0
+assert len(x.raw_leaflets[0][0]) == 167
+assert np.max(list(x.raw_leaflets[0][0])) == 199
+assert np.median(list(x.raw_leaflets[0][0])) == 84.0
+assert np.min(list(x.raw_leaflets[0][0])) == 0
 
 assert len(x.leaflets) == len(x.sim)
-assert len(x.leaflets[0]["upper"]) == 168
+assert len(x.leaflets[0]["upper"]) == 167
 assert np.max(x.leaflets[0]["upper"]) == 199
-assert np.median(x.leaflets[0]["upper"]) == 83.5
+assert np.median(x.leaflets[0]["upper"]) == 84.0
 assert np.min(x.leaflets[0]["upper"]) == 0
-
